915f4d4e47727321c60cf0addc6995f7fd0fe941
alexhpatel <alexanderpatel@college.harvard.edu>
2013-11-30 23:52:41 -0500

    aws shit

--- application.py
+++ application.py
@@ -1,12 +1,23 @@
-from flask import Flask
-from markov import joke
+#from flask import Flask
+#from markov import joke
 
-application = Flask(__name__)
-application.config.from_object(__name__)
+#application = Flask(__name__)
+#application.config.from_object(__name__)
 
+#@application.route('/')
+#def main():
+#	return joke()
+
+#if __name__ == '__main__':
+#	application.run(host='0.0.0.0', debug=True)
+
+import flask
+ 
+application = flask.Flask(__name__)
+ 
 @application.route('/')
-def main():
-	return joke()
-
+def hello_world():
+    return "Hello world!"
+ 
 if __name__ == '__main__':
-	application.run(host='0.0.0.0', debug=True)
+    application.run(host='0.0.0.0')
