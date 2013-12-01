144b854ed077eb3f49771ea1c497b649dca61556
alexhpatel <alexanderpatel@college.harvard.edu>
2013-11-30 23:56:28 -0500

    more aws

--- application.py
+++ application.py
@@ -12,12 +12,13 @@
 #	application.run(host='0.0.0.0', debug=True)
 
 import flask
+from markov import joke
  
 application = flask.Flask(__name__)
  
 @application.route('/')
-def hello_world():
-    return "Hello world!"
+def main():
+    return joke()
  
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
