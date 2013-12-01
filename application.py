8ba96cba928626d0fe45c9229fc4703c5fb6f9fc
alexhpatel <alexanderpatel@college.harvard.edu>
2013-12-01 00:10:57 -0500

    initial

--- application.py
+++ application.py
@@ -1,26 +1,14 @@
-#from flask import Flask
-#from markov import joke
-
-#application = Flask(__name__)
-#application.config.from_object(__name__)
-
-#@application.route('/')
-#def main():
-#	return joke()
-
-#if __name__ == '__main__':
-#	application.run(host='0.0.0.0', debug=True)
-
 import flask
-from markov import *
  
 application = flask.Flask(__name__)
 
+#Set application.debug=true to enable tracebacks on Beanstalk log output. 
+#Make sure to remove this line before deploying to production.
 application.debug=True
  
 @application.route('/')
 def hello_world():
-	return "hello world"
+    return "Hello Alex!"
  
 if __name__ == '__main__':
-    application.run(host='0.0.0.0',  debug=True)
+    application.run(host='0.0.0.0', debug=True)
