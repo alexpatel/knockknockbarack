08545a579c2de6279a4c7e31cb253b93a6015260
alexhpatel <alexanderpatel@college.harvard.edu>
2013-11-30 22:00:14 -0500

    chicken crosses the road

--- markov.py
+++ markov.py
@@ -1,5 +1,6 @@
 from config import connect
 from random import randint	
+from nltk import word_tokenize, pos_tag
 words = connect()
 
 ## generate a markov
@@ -54,9 +55,9 @@ def your_mama():
 	# generate a phrase starting with a 'that' that proceeds an adjective
 	docs = words.find({'pos': 'JJ', '2':'that'})
 	start = docs[randint(0, docs.count() - 1)]['3']
-	phrase = generate(start_word=start)
+	phrase = generate(start)
 
-	print u"Your mama is so {0} that {1}".format(adj, phrase)
+	return u"Your mama is so {0} that {1}".format(adj, phrase)
 
 # generate knock_knock joke
 def knock_knock():	
@@ -64,15 +65,26 @@ def knock_knock():
 	lead = words.find({'pos': 'NN'})[randint(0, words.find({'pos': 'NN'}).count() - 1)]['1'].strip('.')
 
 	# generate phrase from lead
-	phrase = generate(start_word=lead) 
+	phrase = generate(lead) 
 
 	# format for knock-knock format
  	lead = lead.capitalize().strip(',').strip('.')
  	phrase = phrase.capitalize()
 
 	# \n = \u000A. i can't figure out this utf-8 garbage with mongo
-	print u"Knock Knock... \u000AWho's There? \u000A{0}. \u000A{0} who? \u000A{1} \u000A".format(lead, phrase)
+	return u"Knock Knock... \u000AWho's There? \u000A{0}. \u000A{0} who? \u000A{1} \u000A".format(lead, phrase)
 
 # why did the chicken cross the road?
-def chicken()
+def chicken():
+	start = words.find({'1': 'to'})[randint(0, words.find({'1': 'to'}).count() - 1)]['2'].strip('.')
+	# we want to find 'to' --> verb
+	while not pos_tag(word_tokenize(start))[0][1].startswith('V'):
+		start = words.find({'1': 'to'})[randint(0, words.find({'1': 'to'}).count() - 1)]['2'].strip('.')
+
+	phrase = generate(start)
+
+	return u"Why did the chicken cross the road?\u000ATo {0}".format(phrase)
+
+for i in range(20):
+	print chicken()
 
